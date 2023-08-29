30fb8af42870614a5989964746d4a339d42678f4
ong-ar <reclusis@gmail.com>
2023-08-29 17:50:03 +0900

    Remove localstorage logic

--- packages/use-wallets/src/useCosmosWallets.ts
+++ packages/use-wallets/src/useCosmosWallets.ts
@@ -12,10 +12,8 @@ export default function useCosmosWallets() {
 
       if (wallet) {
         setCurrentWallet(wallet);
-        localStorage.setItem('__cosmosWallets', id);
       } else {
         setCurrentWallet(null);
-        localStorage.removeItem('__cosmosWallets');
       }
     },
     [cosmosWallets, setCurrentWallet]
@@ -32,21 +30,6 @@ export default function useCosmosWallets() {
       window.removeEventListener('__cosmosWallets', walletHandler);
     };
   }, [walletHandler]);
-
-  useEffect(() => {
-    const wallets = getCosmosWallets();
-    setCosmosWallets(wallets);
-
-    const savedWalletName = localStorage.getItem('__cosmosWallets');
-    const wallet = cosmosWallets.find((w) => w.name === savedWalletName);
-
-    if (wallet) {
-      setCurrentWallet(wallet);
-    } else {
-      localStorage.removeItem('__cosmosWallets');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return { cosmosWallets, currentWallet, selectWallet };
 }
